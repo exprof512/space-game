24dedb721b4c4d922ecf356eaf3b517456d5a6d1
Юрий <exprof512@gmail.com>
2022-07-09 23:55:42 +0300

    space-game

---
+++ gun.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Gun():
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load('images/pixil-frame-0.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    def output(self):
+        self.screen.blit(self.image, self.rect)
